657afe59b2fb34ed6defbdb08b6e112f811144f4
William Joel Clavell <williamclavell@Williams-Air.fios-router.home>
2020-06-15 16:15:06 -0400

    added a realtime analog clock

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+//clock
+const secondHand = document.querySelector('.second-hand');
+    const minuteHand = document.querySelector('.min-hand');
+    const hourHand = document.querySelector('.hour-hand');
+    const hand = document.querySelector('.hand');
+
+    function setDate () {
+      const now = new Date();
+
+      const seconds = now.getSeconds();
+      const secondsDegrees = ((seconds / 60) * 360) + 90;
+      secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+      const minutes = now.getMinutes();
+      const minutesDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+      minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+      const hours = now.getHours();
+      const hoursDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
+      hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    }
+    //setDate function will run every second
+    setInterval(setDate, 1000);
+
+    setDate();
+
+
+//weather app
 window.addEventListener("load", () => {
 	let long;
 	let lat;
@@ -8,11 +37,13 @@ window.addEventListener("load", () => {
 	const temperatureSpan = document.querySelector(".temperature span");
 	let windRain = document.querySelector(".wind-rain");
 
+//if location is on then this will run
 	if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(position => {
 				long = position.coords.longitude;
 				lat = position.coords.latitude;
 
+		//darksky api and a proxy server to allow us to use it on our device
 				const proxy = "https://cors-anywhere.herokuapp.com/";
 				const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
 
@@ -21,6 +52,7 @@ window.addEventListener("load", () => {
 			return response.json();
 		})
 		.then(data => {
+			//grab this data from the 'currently' array of the api
 			const { temperature, summary, icon, precipProbability, windSpeed } = data.currently;
 			//set DOM elements from the API
 			temperatureDegree.textContent = temperature.toFixed(2);
@@ -49,7 +81,6 @@ window.addEventListener("load", () => {
 	});
 
 }
-
 	function setIcons(icon, iconID) {
 		const skycons = new Skycons({ color: "white" });
 		const currentIcon = icon.replace(/-/g, "_").toUpperCase();
